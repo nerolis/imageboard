b8a20caea0372cde265be0c16aff8310795c109d
nerolis <yakumoft@gmail.com>
2017-06-07 16:44:48 +0300

    loading for home page

--- client/src/components/Home/HomeList.js
+++ client/src/components/Home/HomeList.js
@@ -5,11 +5,29 @@ import SubView from './SubView';
 class HomeList extends Component {
   render() {
       const {threads, posts, fetchPost, fetchThread} = this.props;
+      
+      /// Добавить обработку ошибки. Думаю, реализация через if else return
+       const emptyMessage = (
+            <Message icon>
+        <Icon name='circle notched' loading />
+        <Message.Content>
+            <Message.Header>Just one second</Message.Header>
+            We are fetching that content for you.
+        </Message.Content>
+        </Message>
+  );
+   
     return (
+        <div>
         <div> 
             {threads.slice(-3).map( thread => <HomeView thread={thread} key={thread._id}/>)}    
             {posts.slice(-3).map( post => <SubView post={post} key={post._id} threads={this.props.threads}    />)}   
         </div>
+            <div>
+             {threads.length === 0 ? emptyMessage : HomeList}
+             {posts.length === 0 ? emptyMessage : HomeList}
+         </div>
+         </div>
     );
   }
 }
